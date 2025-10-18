4945f38ea16ca9c9f063bb22e22b6cbee50eb099
TalhaShan <39231024+TalhaShan@users.noreply.github.com>
2025-10-18 19:01:34 +0300

    BS

---
+++ src/main/java/Trees/FindMinInRotatedSortedArray.java
@@ -0,0 +1,34 @@
+package tree;
+
+public class FindMinInRotatedSortedArray {
+
+    public int findMin(int[] arr) {
+        int n = arr.length-1;
+
+        int low = 0;
+        int high = n;
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {  //low <=high means to handle case for 2 elements as well in array see Striver's SDE yt
+            int mid = low + (high - low) / 2;
+            //Optimization extra
+            if(arr[low]<=arr[high]){
+                ans = Math.min(ans,arr[low]);
+                break;
+            }
+            if (arr[low] <= arr[mid]) {
+                //left sorted eliminate left and pick low
+                ans = Math.min(ans, arr[low]);
+                low = mid + 1;
+            } else {
+                //if not left then right is sorted , that's property of rotated sorted array
+                ans = Math.min(ans, arr[mid]);
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
