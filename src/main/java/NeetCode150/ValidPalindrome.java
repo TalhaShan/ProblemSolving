bc3b3c696b5cf69fe63050cba09f58230938f2c7
TalhaShan <39231024+TalhaShan@users.noreply.github.com>
2026-03-24 10:24:07 +0200

    Easy

---
+++ src/main/java/NeetCode150/ValidPalindrome.java
@@ -0,0 +1,25 @@
+package NeetCode150;
+
+public class ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){ //lower case must
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
