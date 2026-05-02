8b5f62d48f21285cc0f19bc634b7ab0ab248ff75
TalhaShan <39231024+TalhaShan@users.noreply.github.com>
2026-05-02 22:11:56 +0300

    More on medium

---
+++ src/main/java/NeetCode150/DailyTemperature.java
@@ -0,0 +1,19 @@
+package NeetCode150;
+
+import java.util.Stack;
+
+public class DailyTemperature {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[temperatures.length];
+        for(int i=0; i<temperatures.length; i++){
+            while (!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]) {
+                int index = stack.pop();
+                ans[index] = i-index;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
