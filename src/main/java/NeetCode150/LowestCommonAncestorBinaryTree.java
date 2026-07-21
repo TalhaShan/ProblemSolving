bea7af7691ddb65a700b8887f3fb2011c1021b2c
TalhaShan <39231024+TalhaShan@users.noreply.github.com>
2026-07-21 23:54:05 +0300

    Add files via upload

---
+++ src/main/java/NeetCode150/LowestCommonAncestorBinaryTree.java
@@ -0,0 +1,34 @@
+package NeetCode150;
+
+public class LowestCommonAncestorBinaryTree {
+
+    public class TreeNode {
+        int val;
+         TreeNode left;
+         TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val,  TreeNode left,  TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        int parentval = root.val;
+        int pVal= p.val;
+        int qVal = q.val;
+
+        if(pVal > parentval && qVal > parentval){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if(pVal<parentval && qVal<parentval){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        else{
+            return root;
+        }
+    }
+}
