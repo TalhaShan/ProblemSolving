dfeb286e36bdf9c4a6c2263ec51d68707260a118
TalhaShan <39231024+TalhaShan@users.noreply.github.com>
2026-09-12 15:00:13 +0300

    Heap practice

---
+++ src/main/java/NeetCode150/KthDistanceFromOrigin.java
@@ -0,0 +1,29 @@
+package NeetCode150;
+
+import java.util.PriorityQueue;
+
+public class KthDistanceFromOrigin {
+    public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (Integer.compare(distance(b), distance(a))));
+
+        for (int[] point : points) {
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll();
+        }
+
+        return result;
+    }
+
+    public int distance(int[] point) {
+
+        return point[0] * point[0] + point[1] * point[1];
+    }
+}
